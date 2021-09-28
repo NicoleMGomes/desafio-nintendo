8895131a1fd1140e8cd9fec41c76695c65a4d010
Isadora Guedes <isasoaresguedes@gmail.com>
2021-09-28 20:26:05 -0300

    Altera menu inicial

--- components/ImageTransform/index.tsx
+++ components/ImageTransform/index.tsx
@@ -57,16 +57,12 @@ const ImageTransform: React.FC = () => {
           name="select"
           onChange={(e) => onChangeSelect(e)}
         >
-          <option value="espelhar_verticalmente">Espelhar verticalmente</option>
-          <option value="espelhar_horizontalmente">
-            Espelhar horizontalmente
-          </option>
-          <option value="deslocar_horizontalmente">
-            Deslocar horizontalmente
-          </option>
-          <option value="aumentar">Aumentar</option>
-          <option value="reduzir">Reduzir</option>
-          <option value="rotacionar">Rotacionar</option>
+          <option value="grayscale_gauss">Grayscale + Gauss</option>
+          <option value="brilho">Brilho 0.8</option>
+          <option value="deteccao_borda">Detector de borda</option>
+          <option value="contraste">Contraste 0.7</option>
+          <option value="mediana">Mediana local</option>
+          <option value="negativo">Negativo</option>
         </SelectEfeito>
 
         <button onClick={submitImage} disabled={!url}>
